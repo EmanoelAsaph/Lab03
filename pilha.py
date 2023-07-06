c6e622d04f62f4f3555207b885715d0914c59856
EmanoelAsaph <117940446+EmanoelAsaph@users.noreply.github.com>
2023-07-06 16:15:08 -0300

    Create pilha.py

---
+++ pilha.py
@@ -0,0 +1,14 @@
+from lista_ligada import LinkedList
+
+class pilha(LinkedList):
+  def __init__(self):
+    self.inicio = None
+
+def push(self,valor):
+  self.pilha.inserir_inicio(valor)
+
+def pop(self,valor):
+  self.pilha.remove(0)
+
+def search(self,valor):
+  self.pilha.procura(valor)
