1d1f23aa9e11d2b4626f4684334b17eceb34bf69
EmanoelAsaph <117940446+EmanoelAsaph@users.noreply.github.com>
2023-07-06 16:15:34 -0300

    Create fila.py

---
+++ fila.py
@@ -0,0 +1,14 @@
+from lista_ligada import LinkedList
+
+class fila(LinkedList):
+  def __init__(self):
+    self.inicio = None
+
+def enfila(self,valor):
+  self.fila.inserir_fim(valor)
+
+def desenfila(self,valor):
+  self.fila.remove(0)
+
+def search(self,valor):
+  self.fila.procura(valor)
