63bb882917b2ddd3ef83e39fd2bae82434539e54
EmanoelAsaph <117940446+EmanoelAsaph@users.noreply.github.com>
2023-07-06 16:23:41 -0300

    Update lista_ligada.py

--- lista_ligada.py
+++ lista_ligada.py
@@ -16,6 +16,7 @@ class No:
 class LinkedList(estrutura_elementar):
     def __init__(self):
         self.inicio = None
+
     def inserir_inicio(self, valor):
         if self.inicio is None:
             self.inicio = No(valor)
@@ -46,7 +47,8 @@ class LinkedList(estrutura_elementar):
         
         elif self.inicio.valor == item:
             self.inicio = self.inicio.get_proximo()
-         else:
+
+        else:
             aux = self.inicio
             
             while aux.get_proximo() is not None:
@@ -55,6 +57,9 @@ class LinkedList(estrutura_elementar):
                 
                 else:    
                     aux = aux.get_proximo()
+                
+
+
     def tamanho(self) -> int:
         if self.inicio is None:
             return 0
@@ -68,14 +73,78 @@ class LinkedList(estrutura_elementar):
 
     def limpa(self):
         self.inicio = None
+
     def procura(self, item) -> bool:
-    
-            
+        
+        if self.inicio == None:
+            return False
+        
+        elif self.inicio.valor == item:
+            return True
+        
+        else:
+            aux = self.inicio
+            while aux.get_proximo() is not None:
+                if aux.get_proximo().valor == item:
+                    return True
+                else:
+                    aux= aux.get_proximo()
+                
         return False
 
-    def indice_de(self, item): 
-        if self.inicio is None:
-            return-1
+    def indice_de(self, item):
+        if self.inicio == None:
+            return -1
+        
+        elif self.inicio.valor== item:
+            return 0
+        
+        else:
+            aux = self.inicio
+            cont = 1
+            while aux.get_proximo() is not None:
+                if aux.get_proximo().valor == item:
+                    return cont
+                else:
+                    aux = aux.get_proximo()
+                    cont += 1
+
+            return -1
+
+    def recupera_indice(self, index):
+        if self.inicio == None:
+            return None
+        elif index == 0:
+            return self.inicio.valor
+        else:
+            aux = self.inicio
+            cont = 0
+            while aux is not None:
+                if cont == index:
+                    return aux.valor
+                cont += 1
+                aux = aux.get_proximo()
+            
+            return None
+    
+    def remove_indice(self, index):
+        if self.inicio == None:
+            return None
+        elif index == 0:
+            self.inicio = self.inicio.get_proximo()
+        else:
+            cont=1
+            aux = self.inicio
+            while aux.get_proximo() is not None:
+                if index == cont:
+                    aux.set_proximo(aux.get_proximo().get_proximo())
+                else:
+                    aux = aux.get.proximo()
+                    cont += 1
+
+       
+
+
 
        
      
